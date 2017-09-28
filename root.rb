d1bfda13bd03ebc83be1880152e176f5b2728be8
Lindsay Dages <lindsaykayedutton@gmail.com>
2017-09-28 17:14:36 +0000

    updated root

--- root.rb
+++ root.rb
@@ -1,8 +1,15 @@
 # gets project root
-project_root = File.dirname(File.absolute_path(__FILE__))
+# project_root = File.dirname(File.absolute_path(__FILE__))
 
 # include models
-Dir.glob(project_root + '/models/*') { |file| require file }
+# Dir.glob(project_root + '/models/*') { |file| require file }
+require './models/app.rb'
+require './models/welcome.rb'
+require './models/player.rb'
+require './models/character.rb'
+require './models/gibbon.rb'
+require './models/newt.rb'
+
 
 # include first prompt
 require './prompts/setup_game'
